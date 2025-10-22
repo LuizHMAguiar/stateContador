5fe7de7be085c33df1af549bd7e599ff0c3297d6
LuizHMAguiar <1397749@etfbsb.edu.br>
2025-10-22 11:08:15 -0300

    Códido simplificado.

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,23 @@ import './App.css'
 
 function App() {
   const [contador, setContador] = useState(0);
-  let mostrar = false;
-  const mensagem = "Você conseguiu!";
-
-  if(contador === 19) {
-    mostrar = true;
-  }
-  else{
-    mostrar = false;
-  }
-
+  const mensagemSucesso = "Você conseguiu!";
+  const mensagemErro = "Tente mais um pouco!";
 
   return (
     <>
       <div className="card">
         <h2>O valor está em {contador}</h2>
       
-        <button onClick={() => setContador((contador) => contador + 3)}>
+        <button onClick={() => setContador(contador + 3)}>
           Somar 3
         </button>
-        <button onClick={() => setContador((contador) => contador -2)}>
+        <button onClick={() => setContador(contador - 2)}>
           Diminuir 2
         </button>
 
-        {mostrar && <h1>{mensagem}</h1>}
+      
+        {contador===19? <h1>{mensagemSucesso}</h1> : <h1>{mensagemErro}</h1>}
       </div>
     </>
   )
